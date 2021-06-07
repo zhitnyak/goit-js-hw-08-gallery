5c29f4a33b9f3a84f8acf153269cecadeed55f6f
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-06-07 21:44:42 +0300

    2_task_test

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,17 @@
 import images from "./gallery-items.js";
 
+//1.2.зарендерим разметку в контейнер galleryBox ч/з insertAdjacentHTML в ul
 const galleryBox = document.querySelector(".js-gallery");
 const imgMarkup = createGallery(images);
 
 galleryBox.insertAdjacentHTML("beforeend", imgMarkup);
 
-// function createImg(images) {}
-// console.log(createGallery(images));
+//2.1вешаем на на контейнер слушателя
+galleryBox.addEventListener("click", onBoxClick);
 
+//1создаем разметку, заполняем HTML шаблон
 function createGallery(images) {
+  //1.1.возвращаем карточку с разметкой
   return images
     .map(({ preview, original, description }) => {
       return `
@@ -28,5 +31,24 @@ function createGallery(images) {
     })
     .join("");
 }
+//2.реализуем делегирование
+function onBoxClick(evt) {
+  //2.1.если это не элемент gallery__image, то мы выходит их ф-ции
+  const ImgEl = evt.target.classList.contains(".gallery__image");
+  if (!ImgEl) {
+    return;
+  }
+  //2.2.теперь нужно найти и выбрать текущее изображение (сначала нужно снять активный класс - т.к он может быть уже выбран)
+  const currentActivEl = document.querySelector(".gallery__item.is-active");
+  //2.3.пишем услоевие - если есть такой класс, то удаляем его
+  if (currentActivEl) {
+    currentActivEl.classlist.remove("is-active");
+  }
 
-// console.log(createCardsMarkcup)
+  //2.4.исходный эл-т, на котором произошло событие
+  const galeryEl = evt.target;
+  //2.5.ищем предка с аналогичным селектором
+  const parentEl = galeryEl.closest(".gallery__item");
+  //2.6.на этого найденного предка добавляем класс
+  parentEl.classList.add("is-active");
+}
